75733ebfdd0a49ac8c2361c887b3b92acc28d5e9
jasonz0704 <oonetwome@gmail.com>
2026-01-05 14:01:52 -0500

    2

--- app/src/main.js
+++ app/src/main.js
@@ -1,24 +1,14 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+async function getData() {
+  try {
+    const response = await fetch(`https://www.freetogame.com/api/${games}`);
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      data.forEach((card) => console.log(card));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData();
