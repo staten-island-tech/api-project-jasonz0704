da2b2429f3039d7db404ed9f679c8c7e99485af0
jasonz0704 <oonetwome@gmail.com>
2026-01-06 14:01:42 -0500

    Update main.js

--- app/src/main.js
+++ app/src/main.js
@@ -1,14 +1,30 @@
-async function getData() {
+/* async function getData() {
   try {
-    const response = await fetch(`https://www.freetogame.com/api/${games}`);
+    const response = await fetch(`https://www.freetogame.com/api/games`);
     if (response.status != 200) {
       throw new Error(response);
     } else {
       const data = await response.json();
-      data.forEach((card) => console.log(card));
+      document.getElementById("api-response").textContent = data.name;
     }
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 }
 getData();
+ */
+
+const URL = "https://official-joke-api.appspot.com/random_joke";
+
+async function getData(URL) {
+  const response = await fetch(URL);
+  console.log(response);
+  /* try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data.content);
+  } catch (error) {
+    console.log(error);
+  } */
+}
+getData(URL);
